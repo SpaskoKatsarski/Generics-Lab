f4989c815692397fd570333edc58e3fe7f895a47
Spasko Katsarski <patsokatsarski1@gmail.com>
2022-06-09 14:30:40 +0300

    Add project files.

---
+++ Box/Box.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private List<T> list = new List<T>();
+
+        public int Count => list.Count;
+
+        public void Add(T element)
+        {
+            list.Add(element);
+        }
+
+        public T Remove()
+        {
+            T removedElement = this.list[this.Count - 1];
+            list.RemoveAt(this.Count - 1);
+
+            return removedElement;
+        }
+    }
+}
---
+++ Box/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BoxOfT
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Box<int> box = new Box<int>();
+
+            box.Add(2);
+            box.Add(0);
+            box.Add(3);
+            box.Add(1);
+
+            Console.WriteLine(box.Count);
+            Console.WriteLine(box.Remove());
+            Console.WriteLine(box.Count);
+
+            Box<bool> box2 = new Box<bool>();
+            box2.Add(2 > 1);
+        }
+    }
+}
